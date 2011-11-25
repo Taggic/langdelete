0a97371b16793cb42916b82f4334150e3b059659
Taggic <taggic@t-online.de>
2011-11-25 10:53:57 +0100

    some minor language corrections
    Signed-off-by: Taggic <taggic@t-online.de>

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -5,11 +5,11 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author Taggic <taggic@t-online.de>
  */
-$lang['admin_langdelete']   = 'Sprachdateien löschen';
-$lang['p_include']          = 'Bitte überlegen Sie genau, welche Sprachen Sie löschen, eine Undo Funktion gibt es nicht.';
-$lang['i_choose']           = 'Bitte tragen Sie hier die ISO-Codes der Sprachen ein, welche Sie ausser Englisch behalten möchten. <br />Wenn Sie meherere behalten möchten, dann trennen Sie die jeweiligen ISO-Codes durch ein Komma voneinander. <br />';
+$lang['admin_langdelete']   = 'Sprachdateien l&ouml;schen';
+$lang['p_include']          = 'Bitte &uuml;berlegen Sie genau, welche Sprachen Sie l&ouml;schen, eine Undo Funktion gibt es nicht.';
+$lang['i_choose']           = '<br /><br />Bitte tragen Sie hier die ISO-Codes der Sprachen ein, welche Sie ausser Englisch behalten m&ouml;chten. M&ouml;chten Sie nur Englisch behalten, dann tragen Sie bitte <i>en</i> ein. Ein leeres Eingabefeld verhindert die Ausf&uuml;hrung der Funktion, um ein L&ouml;schen wegen Fehlbedienung zu verhindern. Wenn Sie meherere behalten m&ouml;chten, dann trennen Sie die jeweiligen ISO-Codes durch ein Komma voneinander. <br />';
 $lang['i_legend']           = 'Eingabe';
 $lang['i_dryrun']           = 'nur Simulation';
 $lang['btn_start']          = 'Start';
-$lang['langdelete_willmsg'] = 'Die folgenden Ordner würden gelöscht werden.';
-$lang['langdelete_delmsg']  = 'Die folgenden Datein und Ordner wurden gelöscht.';
+$lang['langdelete_willmsg'] = 'Die folgenden Ordner w&uuml;rden gel&ouml;scht werden.';
+$lang['langdelete_delmsg']  = 'Die folgenden Datein und Ordner wurden gel&ouml;scht.';
--- lang/en/lang.php
+++ lang/en/lang.php
@@ -7,7 +7,7 @@
  */
 $lang['admin_langdelete']   = 'Delete languages';
 $lang['p_include']          = 'Please take care that you select properly, which languages to be kept. There does not exist an Undo function.';
-$lang['i_choose']           = 'Please insert here the ISO-Codes for languages you want to keep (beside English).  <br />Please separate the ISO-codes by comma if you want to keep multiple languages. <br />';
+$lang['i_choose']           = 'Please insert here the ISO codes for languages you want to keep (beside English). In the event you want to keep English only then <br />please insert <i>en</i>. An empty input field will not cause any effect to prevent any misuse/operation error. Please separate the ISO <br />language codes by comma if you want to keep multiple languages. <br />';
 $lang['i_legend']           = 'Input';
 $lang['i_dryrun']           = 'simulate only';
 $lang['btn_start']          = 'Start';
