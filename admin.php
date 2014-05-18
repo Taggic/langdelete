0ec15aae28d893c8c51b2c40bea5e046c941477f
Satoshi Sahara <sahara.satoshi@gmail.com>
2014-05-18 12:22:45 +0900

    adjust coding style and utf-8

--- admin.php
+++ admin.php
@@ -5,11 +5,11 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Taggic <taggic@t-online.de>
  */
-/******************************************************************************/
+
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
 
-/******************************************************************************/
+
 /**
  * All DokuWiki plugins to extend the admin function
  * need to inherit from this class
@@ -28,21 +28,21 @@ class admin_plugin_langdelete extends DokuWiki_Admin_Plugin {
             'url'    => 'http://dokuwiki.org/plugin:langdelete',
         );
     }
-/******************************************************************************/
+
     /**
      * return prompt for admin menu
      */
     function getMenuText($language) {
         return $this->getLang('admin_langdelete');
     }
-/******************************************************************************/
+
     /**
      * return sort order for position in admin menu
      */
     function getMenuSort() {
         return 20;
     }
-/******************************************************************************/
+
     /**
      * handle user request
      *
@@ -53,7 +53,7 @@ class admin_plugin_langdelete extends DokuWiki_Admin_Plugin {
     function handle() {
         global $ID;
     }
-/******************************************************************************/
+
     /**
      * langdelete Output function
      *
@@ -86,20 +86,13 @@ class admin_plugin_langdelete extends DokuWiki_Admin_Plugin {
             else {
               echo '<br /><div class="level4"><strong><div class="it__standard_feedback">'.$this->getLang('langdelete_delmsg').'</div></strong></div><br />';
             }
-            
-            $lang_keep = $lang_keep.",en";
-            $lang_keep = explode(",",$lang_keep);
-            // clean-up from leading and ending space characters
-            foreach ($lang_keep as &$f) {
-                $f = "/lang/".trim($f);
-            }
-            unset($f); 
-/*            foreach ($lang_keep as $f) {
-                echo "|".$f."|<br />";
-            } */
+
+            $lang_keep = $lang_keep.',en';
+            $lang_keep = explode(',', $lang_keep);
+
             echo '<br /><div class="level4">';
-            $this->_list_language_dirs(DOKU_INC."inc", 0, $lang_keep, $dryrun);
-            $this->_list_language_dirs(DOKU_INC."lib", 0, $lang_keep, $dryrun);
+            $this->_list_language_dirs(DOKU_INC.'inc', 0, $lang_keep, $dryrun);
+            $this->_list_language_dirs(DOKU_INC.'lib', 0, $lang_keep, $dryrun);
             echo '</div>';
         }
 
@@ -111,11 +104,11 @@ class admin_plugin_langdelete extends DokuWiki_Admin_Plugin {
         echo '</div>'.NL;
 
     }
-/******************************************************************************/
+
     /**
      * Display the form with input control to let the user specify,
      * which languages to be kept beside en
-     *     
+     *
      * @author  Taggic <taggic@t-online.de>
      */
     function _html_detail(){
@@ -141,81 +134,75 @@ class admin_plugin_langdelete extends DokuWiki_Admin_Plugin {
         echo  '</form>'.NL;
         echo '</div>'.NL;
     }
-    
-/******************************************************************************/
+
+
   /**
    * This function will read the full structure of a directory. 
    * It's recursive becuase it doesn't stop with the one directory, 
    * it just keeps going through all of the directories in the folder you specify.
    */
-    function _list_language_dirs($path, $level,$lang_keep,$dryrun){ 
-        $ignore = array( 'cgi-bin', '.', '..' );
+    function _list_language_dirs($path, $level,$lang_keep,$dryrun){
         // Directories to ignore when listing output. Many hosts 
-        // will deny PHP access to the cgi-bin. 
-        $dh = @opendir( $path ); 
-        // Open the directory to the handle $dh 
-        while( false !== ( $file = readdir( $dh ) ) ){ 
-        // Loop through the directory          
-            if( !in_array( $file, $ignore ) ){ 
-            // Check that this file is not to be ignored                  
-//                $spaces = str_repeat( '&nbsp;', ( $level * 4 ) ); 
-                // Just to add spacing to the list, to better 
-                // show the directory tree.                  
-                if( is_dir( "$path/$file" ) ){ 
-                // Its a directory, so we need to keep reading down...
+        // will deny PHP access to the cgi-bin.
+        $ignore = array( 'cgi-bin', '.', '..' );
+        // Open the directory to the handle $dh
+        $dh = @opendir( $path );
+        while( false !== ($file = readdir($dh)) ){
+        // Loop through the directory
+            if( !in_array( $file, $ignore ) ){
+                // Check that this file is not to be ignored
+                if( is_dir( "$path/$file" ) ){
+                    // Its a directory, so we need to keep reading down...
                     $cFlag = false;
                     foreach ($lang_keep as $f) {
-                        if (stripos("$path/$file",$f)>0){
+                        if (stripos("$path/$file",$f)>0) {
                             $cFlag = true;
-                            break; }  
+                            break;
+                        }
                     }
-                    if ((stripos("$path/$file","/lang/")>0) && ($cFlag == false)) 
-                    { 
-                      $dir = $path.'/'.$file;
-                      if ($dryrun==true) { 
-                          echo "<strong>".substr($dir,strlen(DOKU_INC),strlen($dir)-strlen(DOKU_INC))."</strong><br />";
-                      }
-                      // now delete the lanuage sub-folder
-                      else {
-                          $this->rrmdir($dir);
-                      }
+                    if ((stripos("$path/$file",'/lang/')>0) && ($cFlag == false)) {
+                        $dir = $path.'/'.$file;
+                        if ($dryrun==true) { 
+                            echo '<strong>'.substr($dir,strlen(DOKU_INC),strlen($dir)-strlen(DOKU_INC)).'</strong><br />';
+                        } else {
+                            // now delete the lanuage sub-folder
+                            $this->rrmdir($dir);
+                        }
+                    }  else {
+                        // Re-call this same function but on a new directory.
+                        // this is what makes function recursive.
+                        $this->_list_language_dirs( "$path/$file", ($level+1), $lang_keep,$dryrun );
                     }
-                    else
-                    {  $this->_list_language_dirs( "$path/$file", ($level+1), $lang_keep,$dryrun );  } 
-                    // Re-call this same function but on a new directory. 
-                    // this is what makes function recursive.                  
-                } 
-            } 
-        }          
-        closedir( $dh ); 
-        // Close the directory handle 
+                }
+            }
+        }
+        closedir( $dh );
+        // Close the directory handle
     }
-/******************************************************************************/
-  /**
-   * This function will delete all folders and files which are in the specified directory.
-   * This is necessary due to only empty directories can be deleted.   
-   */
-  function rrmdir($dir) {
+
+    /**
+     * This function will delete all folders and files which are in the specified directory.
+     * This is necessary due to only empty directories can be deleted.
+     */
+    function rrmdir($dir) {
     // replace "//" in $dir if existing
-    $dir = str_replace("//", "/", $dir);
-    
-    if (is_dir($dir)) { 
-      $objects = scandir($dir); 
-      foreach ($objects as $object) { 
-        if ($object != "." && $object != "..") { 
-           if (filetype($dir."/".$object) == "dir") {
-              $this->rrmdir($dir."/".$object);
-              echo "<strong>".$dir."/".$object." ->directory removed</strong><br />";
-           } 
-           else {
-              unlink($dir."/".$object);
-              echo $dir."/".$object." -> file deleted<br />";
-           }
-        } 
-      } 
-      reset($objects); 
-      rmdir($dir); 
-    } 
-  } 
-/******************************************************************************/
+        $dir = str_replace('//', '/', $dir);
+
+        if (is_dir($dir)) {
+            $objects = scandir($dir);
+            foreach ($objects as $object) {
+                if ($object != '.' && $object != '..') {
+                    if (filetype($dir.'/'.$object) == 'dir') {
+                        $this->rrmdir($dir.'/'.$object);
+                        echo '<strong>'.$dir.'/'.$object.' ->directory removed</strong><br />';
+                    } else {
+                       unlink($dir.'/'.$object);
+                       echo $dir.'/'.$object.' -> file deleted<br />';
+                    }
+                }
+            }
+            reset($objects);
+            rmdir($dir); 
+        }
+    }
 }
