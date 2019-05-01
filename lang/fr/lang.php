402afdee66ebf3ac46fe8f775465bb20c6423f18
Tilwa Qendov <tilwa.qendov@gmail.com>
2019-05-01 13:41:14 +0200

    Add French translations

---
+++ lang/fr/lang.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * english language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author Taggic <taggic@t-online.de>
+ */
+$lang['menu']               = 'Supprimer des langues';
+
+$lang['i_legend']           = 'Saisie';
+$lang['i_using']            = 'Langue utilisée';
+$lang['i_shouldkeep']       = 'Langue(s) à conserver';
+$lang['i_runoption']        = 'Options d\'execution';
+$lang['i_dryrun']           = 'simuler uniquement';
+$lang['btn_start']          = 'Démarrer';
+
+$lang['available_langs']    = 'Langues disponibles :';
+$lang['dokuwiki_core']      = 'Noyau DokuWiki :';
+$lang['templates']          = 'Modèles :';
+$lang['plugins']            = 'Greffons :';
+
+$lang['h2_output']          = 'Resultats';
+$lang['discrepancy_warn']   = 'Désaccord trouvé entre le formulaire et les cases à chocher';
+$lang['nolang']             = 'Ces langues n\'existent pas : ';
+$lang['langdelete_willmsg'] = 'Ces fichiers de langues vont être supprimés';
+$lang['langdelete_delmsg']  = 'Ces fichiers de langues ont étés supprimés';
+$lang['langdelete_attention'] = 'Assurez vous de choisir les bonnes langues. Il n\'y a pas de retour en arrière.';
+$lang['backto_inputbox']    = 'Retour au formulaire';
