0e31b1573681b278bfca9e8c7ee994542265afa6
prschopf <16743256+prschopf@users.noreply.github.com>
2020-06-28 21:54:06 -0300

    Add files via upload

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * english language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author Taggic <taggic@t-online.de>
+ */
+$lang['menu']               = 'Exclusão de linguagens';
+
+$lang['i_legend']           = 'Opções';
+$lang['i_using']            = 'Linguagem default';
+$lang['i_shouldkeep']       = 'Linguagens a manter';
+$lang['i_runoption']        = 'Simulação';
+$lang['i_dryrun']           = 'apenas simular';
+$lang['btn_start']          = 'Iniciar';
+
+$lang['available_langs']    = 'Linguagens disponíveis:';
+$lang['dokuwiki_core']      = 'Núcleo do DokuWiki:';
+$lang['templates']          = 'Modelos:';
+$lang['plugins']            = 'Plugins:';
+
+$lang['h2_output']          = 'Resultados';
+$lang['discrepancy_warn']   = 'Encontrada discrepância entre o formulário de texto e o checkbox';
+$lang['nolang']             = 'As seguintes linguagens não existem: ';
+$lang['langdelete_willmsg'] = 'As seguintes linguagens serão excluídas:';
+$lang['langdelete_delmsg']  = 'As seguintes linguagens foram excluídas:';
+$lang['langdelete_attention'] = 'Certifique-se de ter escolhido as linguagens corretas. Não há como desfazer esta operação.';
+$lang['backto_inputbox']    = 'Voltar às opções';
